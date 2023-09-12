b8bdcc08a28c60f8a1901f260b8a0e82e4cf8b14
Buhle M <mthembbu@gmail.com>
2023-09-12 21:04:15 +0200

    rest template

--- app-spring-security-section-4/src/main/java/com/tstcore/easybank/EasyBankBackEndApp.java
+++ app-spring-security-section-4/src/main/java/com/tstcore/easybank/EasyBankBackEndApp.java
@@ -2,12 +2,19 @@ package com.tstcore.easybank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EasyBankBackEndApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyBankBackEndApp.class, args);
+	}
+	
+	@Bean
+	private RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
